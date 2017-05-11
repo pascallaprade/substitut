cf8f1823ddcd8551b996b47d69d1bc344919a143
Pascal Laprade <laprade.p@gmail.com>
2017-05-11 14:30:21 -0400

    Changed algorithm to recursive
    The algorithm can now safely deal with repetitions in patterns,
    and is also simpler. All tests now pass.

--- test/test.js
+++ test/test.js
@@ -8,23 +8,25 @@ const {
 
 const objectDictionary = {
   'a': '42',
-  'z': 'c',
+  'Z': 'c',
   'c': '()',
   'i': '1',
   '6': 'mail',
-  'car': 'thing'
+  'car': 'thing',
+  ':': '=',
+  'ç': 'ù'
 }
 
 const arrayDictionary = [
-  'a','z','c','i','6','car'
+  'a','Z','c','i','6','car', ':', 'ç'
 ]
 
 describe('In the substitut library,', function() {
   describe('#substitute()', function() {
     it('should return a new string with the patterns found in the dictionary ' +
        'substituted with their associated value', function() {
-      const input = "Hello i am a zi6-model"
-      const expected = "Hello 1 42m 42 c1mail-model"
+      const input = "Hello, i am a zi6-model."
+      const expected = "Hello, 1 42m 42 c1mail-model."
       const result = substitute(input, objectDictionary)
       expect(result).to.equal(expected)
     })
@@ -33,18 +35,25 @@ describe('In the substitut library,', function() {
   describe('#countOccurences()', function() {
     it('should return the correct number of occurences when ' +
        'passed an object dictionary', function() {
-      const input = "Hello i am a zi6-model"
-      const expected = 6
+      const input = "Hello, i am a zi6-model: ça va?"
+      const expected = 10
       const result = countOccurences(input, objectDictionary)
       expect(result).to.equal(expected)
     })
 
-
     it('should return the correct number of occurences when ' +
        'passed an array dictionary', function() {
-      const input = "Hello i am a zi6-model"
-      const expected = 6
+      const input = "Hello, i am a zi6-model: ça va?"
+      const expected = 10
       const result = countOccurences(input, arrayDictionary)
+      expect(result).to.equal(expected)
+    })
+
+    it('should return the correct number of occurences when ' +
+       'the input string contains non-alphabetic characters', function() {
+      const input = '6:6:6'
+      const expected = 5
+      const result = countOccurences(input, objectDictionary)
       expect(result).to.equal(expected)
     })
   })
