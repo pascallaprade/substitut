2bd45d551dc0512c82a4ae8b211968cdb4135e38
Pascal Laprade <laprade.p@gmail.com>
2017-05-10 23:11:06 -0400

    Added first pattern counting method
    - Exposed countOccurences to allow counting occurences of
      patterns from a passed dictionary in a passed string.
    - Wrote tests for object and array variants.

---
+++ test/test.js
@@ -0,0 +1,47 @@
+"use strict"
+
+const expect = require('chai').expect
+const {
+  countOccurences
+} = require('../index')
+console.log((typeof countOccurences))
+
+const objectDictionary = {
+  'a': '42',
+  'z': 'c',
+  'c': '()',
+  'i': '1',
+  '6': 'mail',
+  'car': 'thing'
+}
+
+const arrayDictionary = [
+  'a','z','c','i','6','car'
+]
+
+describe('', function() {
+  describe('#substitute()', function() {
+    it('should ', function() {
+
+    })
+  })
+
+  describe('#countOccurences()', function() {
+    it('should reurn the correct number of occurences when ' +
+       'passed an object dictionary', function() {
+      const input = "Hello i am a zi6-model"
+      const expected = 6
+      const result = countOccurences(input, objectDictionary)
+      expect(result).to.equal(expected)
+    })
+
+
+    it('should reurn the correct number of occurences when ' +
+       'passed an array dictionary', function() {
+      const input = "Hello i am a zi6-model"
+      const expected = 6
+      const result = countOccurences(input, arrayDictionary)
+      expect(result).to.equal(expected)
+    })
+  })
+})
